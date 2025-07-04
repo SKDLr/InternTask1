b0b585e8610765e68318342afe63f6cf6900b792
SKDLr <141830759+SKDLr@users.noreply.github.com>
2025-07-04 05:38:30 +0000

    Saving my work

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    try {
+        res.json('The Backend is Active');
+    } catch (err) {
+        res.status(500).json({ Error: err.message });
+    }
+});
+
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Connected Successfully on PORT ${PORT}`);
+});
